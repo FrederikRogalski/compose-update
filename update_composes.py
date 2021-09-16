f298ca0acaf6e905112c4a5873dd2a0abeacddbf
FrederikRogalski <frederik@musikschule.de>
2021-09-16 11:24:31 +0000

    added script

---
+++ update_composes.py
@@ -0,0 +1,45 @@
+import os
+import click
+import tqdm
+import multiprocessing as mp
+
+compose_names = {"compose.yaml",
+                 "compose.yml",
+                 "docker-compose.yaml",
+                 "docker-compose.yml"}
+
+def update_compose(x):
+    # check if dir exist
+    if not os.path.isdir(x):
+        raise NotADirectoryError(f"'{x}'")
+
+    # check if dir contains compose file
+    compose_file = compose_names.intersection(os.listdir(x))
+    if len(compose_file)==0:
+        raise FileNotFoundError(f"Directory '{x}' doesn't contain a compose file in one of the following forms: {compose_names}")
+    if len(compose_file)>1:
+        raise Exception(f"Directory '{x}' contains more then one compose file, namely: {compose_file}")
+
+    # update images
+    compose_file = compose_file.pop()
+    if os.system(f'docker-compose -f {os.path.join(x, compose_file)} pull') == -1:
+        return -1
+    if os.system(f'docker-compose -f {os.path.join(x, compose_file)} up -d') == -1:
+        return -1
+    return 0
+
+
+@click.command()
+@click.argument('update_dirs', nargs=-1)
+def update_composes(update_dirs):
+    update_dirs_set = set(update_dirs)
+    pool = mp.Pool(mp.cpu_count())
+    
+    results = pool.map(update_compose, update_dirs_set)
+    pool.close()
+
+    print("\nPrune Images?")
+    os.system("docker image prune")
+
+if __name__ == "__main__":
+    update_composes()
